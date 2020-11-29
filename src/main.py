40c84d0dccc581a59972ebf9b6b22770781e2a15
Julius Neudecker <Github@juliusneudecker.com>
2020-11-29 21:16:20 +0100

    rendering working

--- src/main.py
+++ src/main.py
@@ -10,21 +10,17 @@ def main():
     # make the request
     #url = "https://pr0gramm.com"
     url = "https://cms.hotpot.codes/AssetShowcase/Index"
-    #uClient = uReq(url)
-    session = HTMLSession()
-    request = session.get(url)
-    print(request.status_code)
-    #r.headers['content-type']
-    #r.encoding
-    #r.json()
 
-    #render js
-    request.html.render()
-    #print(request.html.text)
-    #content.html.text
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        print("Request yielded: " + str(response.status_code))
+        response.html.render()
+    except requests.exceptions.RequestException as e:
+        print(e)
 
     #parse html
-    page_soup = soup(request.html.text, "html.parser")
+    page_soup = soup(response.html.html, "html.parser")
     print(page_soup)
 
 
